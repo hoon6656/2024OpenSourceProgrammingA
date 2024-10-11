43aac09b053e70f87e6d77970bbfab361e7ecb0b
yh <janny6656@gmail.com>
2024-10-11 11:53:41 +0900

    v1.3 If

--- week07/main.go
+++ week07/main.go
@@ -5,15 +5,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
+	fmt.Print("점수 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
+	score, err := strconv.parseInt(i, 16, 32)
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDEF")
+		fmt.Printf("%d\n", score)
 	}
 }
