317761fe4336dcf518583d4ae95892bdaf5f7dbb
yh <janny6656@gmail.com>
2024-10-11 12:10:11 +0900

    v1.5 aOrNot

--- week07/main.go
+++ week07/main.go
@@ -17,12 +17,12 @@ func main() {
 		log.Fatal(err)
 	}
 	i = strings.TrimSpace(i)
-	score, err := strconv.ParseInt(i, 16, 32) //문자열 변수 i의 값을 정수형(32비트)로 변환, 입력받은 값은 16비트
-	if score >= 60 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", score)
+	score, err := strconv.ParseInt(i, 16, 32) //문자열 변수 i의 값을 정수형(32비트)로 변환, 입력받은 값은 16진수
+	var aOrNot string
+	if score >= 90 {
+		aOrNot = "A"
 	} else {
-		fmt.Println("BCDEF")
-		fmt.Printf("%d\n", score)
+		aOrNot = "BCDEF"
 	}
+	fmt.Printf("%d점은 $s등급입니다.\n", score, aOrNot)
 }
