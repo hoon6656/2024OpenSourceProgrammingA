099705cc872872d42a8d5d4b2224281a720c8540
yh <janny6656@gmail.com>
2024-10-11 11:16:34 +0900

    v1.2 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
-	"time"
+	"os"
 )
 
 func main() {
-	broken := "G# r#cks"
-	replacer := strings.NewReplacer("#", "o")
-	fmt.Println(broken)
-	fmt.Println(replacer.Replace(broken))
-
-	var now time.Time = time.Now()
-	var year int = int(now.Year())
-	var month int = int(now.Month())
-	var day int = int(now.Day())
-
-	fmt.Printf("오늘은 %d년 %d월 %d일\n", year, month, day)
-	fmt.Printf("지금은 %d시 %d분 %d초", now.Hour(), now.Minute(), now.Second())
-
+	in := bufio.NewReader(os.Stdin)
+	fmt.Print("Input your name : ")
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
 }
