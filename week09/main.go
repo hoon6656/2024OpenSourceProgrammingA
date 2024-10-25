2c625e510ee6d79d060a253c5ab0bf0b984ea3c4
yh <janny6656@gmail.com>
2024-10-25 11:24:31 +0900

    v1.8 Guess game

--- week09/main.go
+++ week09/main.go
@@ -16,7 +16,8 @@ func main() {
 	answer := rand.Intn(6) + 1
 	fmt.Printf("%d\n", answer)
 
-	for guesses := 3; guesses > 1; guesses++ {
+	win := false
+	for guesses := 3; guesses > 1; guesses-- {
 		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", guesses)
 		r := bufio.NewReader(os.Stdin)
 		i, err := r.ReadString('\n')
@@ -29,14 +30,21 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(guess)
 
+		fmt.Println(guess)
 		if answer == guess {
 			fmt.Println("정답입니다.")
+			win = true
+			break
 		} else if answer > guess {
 			fmt.Println("입력하신 수는 정답보다 작은 수 입니다. low")
 		} else {
 			fmt.Println("입력하신 수는 정답보다 큰 수 입니다.high")
 		}
+	}
+	if win {
+		fmt.Println("당신이 이겼습니다.")
+	} else {
+		fmt.Println("당신이 졌습니다.")
 	}
 }
