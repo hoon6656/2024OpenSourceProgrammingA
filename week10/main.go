2499f0859d679d354d7a1f265ce8bcf995c1c443
yh <janny6656@gmail.com>
2024-11-01 10:57:00 +0900

    v2.5 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -20,13 +20,19 @@ func main() {
 	i = strings.TrimSpace(i)
 	n, err := strconv.Atoi(i)
 
-	counts := 0
-	for j := 1; j <= n; j++ {
-		if n%j == 0 {
-			counts++
+	var isPrime bool = true
+
+	// 논리적 오류 해결
+	if n < 2 { // 1보다 큰 자연수 중(!!!) 1과 자기 자신만을 약수로 가지는 수
+		isPrime = false
+	} else {
+		for j := 2; j < n; j++ {
+			if n%j == 0 {
+				isPrime = false
+			}
 		}
 	}
-	if counts == 2 {
+	if isPrime {
 		fmt.Printf("%d는(은) 소수입니다", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다", n)
