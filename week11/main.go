74628c56ca4c4211106854a17c184642fe5f6ceb
yh <janny6656@gmail.com>
2024-11-08 11:53:33 +0900

    v3.4

--- week11/main.go
+++ week11/main.go
@@ -1,9 +1,45 @@
 package main
 
-import "week11/greeting"
+import (
+	"fmt"
+	"log"
+	"week11/keyboard"
+)
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		for j := 3; j*j <= n; j = j + 2 {
+			if n%j == 0 {
+				return false
+			}
+			//fmt.Printf("%d ", j)
+		}
+	}
+	return true
+}
 
 func main() {
-	// greeting.Hi("Inha")
-	// greeting.Hello("Harvard")
-	greeting.AllGreetings("Inha")
+	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
+	n1, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("두 번째 정수(끝 값) 입력 : ")
+	n2, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := n1; i <= n2; i++ {
+		if isPrime(i) {
+			fmt.Printf("%d ", i)
+		}
+	}
 }
