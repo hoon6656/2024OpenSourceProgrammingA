e11e5314a8f0cbd16778b4570f41e1b345640613
yh <janny6656@gmail.com>
2024-11-15 10:43:20 +0900

    v3.6 Array

--- week12/main.go
+++ week12/main.go
@@ -10,10 +10,10 @@ func main() {
 	//dates[1] = time.Unix(1447920000, 0)
 	//fmt.Println(dates[1])
 
-	//var dates [3]time.Time = [3]time.Time{time.Unix(1, 0), time.Unix(1447920000, 0), time.Unix(1447920001, 0)}
+	//var dates [3]time.Time = [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1447920001, 0)}
 	//fmt.Println(dates[0], dates[1], dates[2])
 
-	//dates := [3]time.Time{time.Unix(1, 0), time.Unix(1447920000, 0), time.Unix(1447920001, 0)}
+	//dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1447920001, 0)}
 	//fmt.Println(dates[0], dates[1], dates[2])
 
 	//dates := [3]time.Time{
