29f0bb478e87a7123ee29803c6b0f54e07edfab8
yh <janny6656@gmail.com>
2024-11-29 11:55:28 +0900

    v5.0 Struct

--- week14/main.go
+++ week14/main.go
@@ -1,24 +1,42 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float32
+type visitor struct {
+	age  int
+	cost int
+}
+
+func calculateCost(visitors []visitor) int {
+	totalCost := 0
+	for _, v := range visitors {
+		totalCost = totalCost + v.cost
+	}
+	return totalCost
 }
 
 func main() {
-	var student1 student
-	student1.id = 202444024
-	student1.name = "주영훈"
-	student1.gpa = 4.5
-	fmt.Println(student1.gpa)
-	var student2 student
-	student2.id = 202444000
-	student2.name = "joo"
-	student2.gpa = 4.4
-	fmt.Println(student2.id)
+	var numVisitors int
+	fmt.Println("How many visitors? ")
+	fmt.Scanln(&numVisitors)
+
+	var vs []visitor // 구조체 슬라이스 변수
+	vs = make([]visitor, numVisitors)
+
+	for i := 0; i < numVisitors; i++ {
+		var age int
+		fmt.Print("Input age : ")
+		fmt.Scan(&age)
+
+		switch {
+		case age < 12:
+			vs[i] = visitor{age: age, cost: 5000}
+		case age >= 12 && age < 65:
+			vs[i] = visitor{age: age, cost: 10000}
+		default: // over 65 years old
+			vs[i] = visitor{age: age, cost: 7000}
+		}
+	}
+
+	fmt.Printf("Total price is %dwon.", calculateCost(vs))
 }
