07baf44a1a89d9bb276e3606c395dcdce9c41371
yh <janny6656@gmail.com>
2024-11-29 11:33:20 +0900

    v4.9 Struct

--- week14/main.go
+++ week14/main.go
@@ -4,23 +4,21 @@ import (
 	"fmt"
 )
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	ages := make(map[string]int)
-	var name string
-	var age int
-
-	for {
-		fmt.Print("name? (exit to 'q')")
-		fmt.Scanln(&name)
-		if name == "q" {
-			break
-		}
-		fmt.Print("age? ")
-		fmt.Scanln(&age)
-
-		ages[name] = age
-	}
-	for name, age := range ages {
-		fmt.Printf("%s is %d year old.\n", name, age)
-	}
+	var student1 student
+	student1.id = 202444024
+	student1.name = "주영훈"
+	student1.gpa = 4.5
+	fmt.Println(student1.gpa)
+	var student2 student
+	student2.id = 202444000
+	student2.name = "joo"
+	student2.gpa = 4.4
+	fmt.Println(student2.id)
 }
